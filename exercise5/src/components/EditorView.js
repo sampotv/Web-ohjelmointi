a24a4a8990d429593e161be476f2ad7f73deefcd
sampotv <91623423+sampotv@users.noreply.github.com>
2022-02-27 14:19:18 +0200

    Add files via upload

--- exercise5/src/components/EditorView.js
+++ exercise5/src/components/EditorView.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function EditorView(props) {
+const [img, setImg] = useState("")
+const [title, setTitle] = useState("")
+const [price, setPrice] = useState("")
   return (
     <div>
         <h1>EditorView</h1>
+        <h3>Lisää uusi tuote</h3>
         <form>
-            <div>Nimi <input type="text" value={props.title}/></div>
-            <div>Hinta <input type="text" value={props.price}/></div>
-            <button>Tallenna</button>
+            <div>Nimi <input type="text" input value={title} onChange={(event) => setTitle(event.currentTarget.value)}/></div>
+            <div>Hinta <input type="text" input value={price} onChange={(event) => setPrice(event.currentTarget.value)}/></div>
+            <div>Tuotteen kuvan URL<input type="text" value={img} onChange={(event) => setImg(event.target.value)}/></div>
+            <button onClick={()=>props.createProduct({
+                    img,title,price
+                      })}>Tallenna uusi tuote</button>
         </form>
 
-
+                      <div><br></br><h3>Poista tuote</h3></div>
         {props.products.map(p=> <div>{p.title} <button onClick={() => props.onItemDelete(p)}>Poista tuote</button></div>)}
     </div>
   )
--- exercise5/src/components/Products.js
+++ exercise5/src/components/Products.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Products(props) {
   return <div className=''>
   <div className='item'>
-        <div><h1>{props.name}</h1></div>
+        <div><h1>{props.title}</h1></div>
         <img src={props.image} alt=""/>
         <div>{props.price}</div>
         </div>        
